ae764fedcf029793e231cd63e46213ad18bbf90e
Kishore <kishorekannann82@gmail.com>
2025-10-21 20:26:14 +0530

    Commit

--- app.py
+++ app.py
@@ -65,44 +65,6 @@ def edit(id:int):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
